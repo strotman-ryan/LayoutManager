c5a4236a5aad704315566c2264005b90dc958b5b
ryan <ryanstrotman@gmail.com>
2022-05-03 01:57:31 -0400

    components endpoint

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ components = {}
 supportedJsonPrimitives = {"STRING", "NUMBER", "BOOLEAN", "NULL"}
 
 
-@app.route("/component", methods=["POST"])
-def component():
+@app.route("/component/<name>", methods=["GET","POST"])
+def component(name):
     if request.method == "POST":
-        name = request.form["name"]
         value = request.form["value"]
         jsonValue = json.loads(value)
         for _, value in jsonValue.items():
             if value not in supportedJsonPrimitives.union(components.keys()):
                 abort(404)
         components[name] = jsonValue
-    return "It worked"
+        return components
+    
+    if request.method == "GET":
+        return components[name]
+
+@app.route("/allcomponents")
+def getAllComponent():
+    return components
