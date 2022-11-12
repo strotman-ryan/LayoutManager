d2443420d37cdabd142666d51beae0d298c1748d
ryan <ryanstrotman@gmail.com>
2022-11-12 14:31:07 -0500

    upgrade to react 18

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,6 +1,8 @@
-import ReactDOM from "react-dom";
 import { App } from "./App";
 import React from "react";
+import { createRoot } from 'react-dom/client';
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App/>);
