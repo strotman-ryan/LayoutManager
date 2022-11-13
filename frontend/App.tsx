aba5e42b82122dd7444bb47f9b2c67718ab8999e
ryan <ryanstrotman@gmail.com>
2022-11-12 19:04:51 -0500

    added types

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import { ComponentFormLoad } from "./ComponetFormLoad";
 
 export function App() {
-  return <ComponentFormLoad />
+  return (
+    <div>
+      <ComponentFormLoad />
+      {/* for numbers */}
+      <input placeholder="numbers only" type="number"></input>
+      {/* for boolean */}
+      <input type="checkbox" value="flase"></input>
+      {/* for string */}
+      <input ></input>
+    </div> 
+  );
 }
 
 //constants
